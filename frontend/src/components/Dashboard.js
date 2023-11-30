474ecf40799d7e8dd0c1ed0cd92d6b4e928d697c
Jingyao Gu <43628019+JingyaoGu1@users.noreply.github.com>
2023-11-30 14:42:52 -0800

    Structure and style change

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,10 +8,11 @@ import FileList from './FileList';
 const Dashboard = ({ user }) => {
   return (
     <div>
+      <div className="container">
       <h1>Welcome to the Dashboard</h1>
       <p>Email: {user.email}</p>
-      <div className="container">
       <h1>Fine-tuned ChatGPT</h1>
+      <h2>You can upload your personal files to fine tune </h2>
       <div className="chatbox">
         <FileUpload />
         <FileList />
